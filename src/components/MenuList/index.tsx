cfba81eecc6f2406239e5edbf14d99bfd56950f6
Samuel <Samuel>
2024-03-20 08:40:24 -0300

    feat: Creating MenuList component

--- src/components/MenuList/index.tsx
+++ src/components/MenuList/index.tsx
@@ -1,34 +1,41 @@
 import "./styles.css";
 
-import images from "../../assets/icons/index.js";
-
 interface MenuListProps {
   title: string;
   description: string;
   price: string;
   imgSrc: string;
+  activeSection: boolean;
+  catchItemAtive: () => void;
 }
 
-const MenuList = ({ title, description, price, imgSrc }: MenuListProps) => {
+const MenuList = ({
+  title,
+  description,
+  price,
+  imgSrc,
+  activeSection,
+  catchItemAtive,
+}: MenuListProps) => {
   return (
-    <div className="menu_list">
-      <div className="menu_list__section-label">
-        <span className="menu_list__section-txt">Burgers</span>
-        <img
-          src={images.arrow}
-          alt="Icon Arrow"
-          className="menu_list__section-icon"
-        />
-      </div>
-      <div className="menu_list__itens">
-        <div className="menu_list__itens-txts">
-          <span className="menu_list__item-product-name">{title}</span>
-          <span className="menu_list__item-description">{description}</span>
-          <span className="menu_list__item-price">{price}</span>
+    activeSection && (
+      <div className="menu_list" onClick={() => catchItemAtive()}>
+        <div className="menu_list__itens">
+          <div className="menu_list__itens-txts">
+            <span className="menu_list__item-product-name">{title}</span>
+            <span className="menu_list__item-description">{description}</span>
+            <span className="menu_list__item-price">{price}</span>
+          </div>
+          {imgSrc && (
+            <img
+              src={imgSrc}
+              alt="Item Image"
+              className="menu_list__itens-img"
+            />
+          )}
         </div>
-        <img src={imgSrc} alt="Item Image" className="menu_list__itens-img" />
       </div>
-    </div>
+    )
   );
 };
 
