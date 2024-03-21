d3094b2be27a56bc639f13aa6a62993e72b65c28
Samuel <Samuel>
2024-03-21 10:04:45 -0300

    feat: New icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,13 +1,15 @@
 import arrow from "./arrow.svg";
 import xicon from "./xicon.svg";
 import searchIcon from "./searchIcon.svg";
+import hamburgerMenu from "./hamburgerMenu.svg";
 
 const images = {
   arrow,
   xicon,
-  searchIcon
+  searchIcon,
+  hamburgerMenu
 };
 
-export { arrow, xicon, searchIcon };
+export { arrow, xicon, searchIcon, hamburgerMenu };
 
 export default images;
