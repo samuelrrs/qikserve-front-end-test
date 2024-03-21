490ea039abad2aac29ae95c6391f374cb5459734
Samuel <Samuel>
2024-03-21 10:06:00 -0300

    feat: Creating store

---
+++ src/store/index.ts
@@ -0,0 +1,18 @@
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer from '../store/modules/rootReducer';
+import rootSaga from '../store/modules/rootSaga';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(sagaMiddleware))
+);
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
