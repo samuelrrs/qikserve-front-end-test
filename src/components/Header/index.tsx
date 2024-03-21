28b259ecd8692119bd4a771d87623859ed24e8b8
Samuel <Samuel>
2024-03-21 10:05:06 -0300

    feat: Responsive layout for header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,21 @@
 import "./styles.css";
+import images from "../../assets/icons/index.ts";
 
 const Header = () => {
   return (
     <header className="header">
       <div className="header__tabs">
-        <span className="header__txts">Menu</span>
+        <span className="header__txts">
+          Menu
+          <div className="header__line-active" />
+        </span>
         <span className="header__txts">Entrar</span>
         <span className="header__txts">Contato</span>
+        <img
+          src={images.hamburgerMenu}
+          alt="Hamburguer Menu Icon"
+          className="header__icon-hamburguer"
+        />
       </div>
       <img
         src="https://preodemo.gumlet.io/usr/venue/7602/web/646fbf3abf9d0.png"
